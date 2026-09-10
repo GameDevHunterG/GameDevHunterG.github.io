5ea42fae4aa87ff3577a1123a9983ce8b503b591
Hunter Gomulkiewicz <thehulksmasher109@gmail.com>
2026-09-10 15:05:49 -0500

    added dynamic welcome message

--- site.js
+++ site.js
@@ -1,1 +1,26 @@
 console.log("Hello there.")
+
+const hours = new Date().getHours();
+
+const isMorning = hours >= 4 && hours < 12;
+const isAfternoon = hours >= 12 && hours < 17;
+const isEvening = hours >= 17 && hours < 4;
+
+const welcome = document.querySelector("#welcome");
+
+const message = document.createElement("h2");
+
+if (isMorning)
+{
+    message.textContent = "Good morning!";
+}
+else if (isAfternoon)
+{
+    message.textContent = "Good afternoon!";
+}
+else
+{
+    message.textContent = "Good evening!";
+}
+
+welcome.append(message);
